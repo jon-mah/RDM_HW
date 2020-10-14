02f0ce1c3a85711a24cca5b145a84100804035c3
Jonathan Mah <jonmah@uw.edu>
2020-10-14 09:21:17 -0700

    Simulation scripts, chromosomes.

---
+++ Scripts/recessive_deleterious_mutations_array.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -t 1:500
+#$ -cwd
+#$ -V
+#$ -m a
+#$ -l h_data=10G
+#$ -l h_rt=0:30:00
+
+# INPUT ARGUMENTS
+seed=1
+
+# If `SLiM` is not an executable, then comment out the next line.
+slim -d chrom=$SGE_TASK_ID -d init_seed=$seed recessive_deleterious_mutations.slim
+
+# If `SLiM` is not an executable, then uncomment the next line and
+# provide a path to `SLiM`.
+# ./slim -d chrom=$SGE_TASK_ID -d init_seed=$seed AW_to_neutral_simulation.slim
