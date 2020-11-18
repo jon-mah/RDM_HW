d3210226585e510801d455e8c115a4b4dfdd9b22
Jonathan Mah <jonmah@uw.edu>
2020-11-18 12:40:39 -0800

    condense vcf bash file.

--- Scripts/condense_vcf.bash
+++ Scripts/condense_vcf.bash
@@ -572,3 +572,145 @@ do
   sed -i -r "s/^1\t/${i}\t/g" ${prefix}_chrom_${i}.vcf
   grep ";MT=1\|;MT=2" ${prefix}_chrom_${i}.vcf >> ${prefix}.vcf
 done
+
+# INPUT ARGUMENTS
+s_value=s_0_seed_2
+sample_size=10
+prefix="../Data/${s_value}/sample_${sample_size}" # Output prefix, and input prefix of given vcf
+
+# Merge all `.vcf` files.
+
+cp ${prefix}_chrom_1.vcf ${prefix}.vcf
+
+for i in {2..400}
+do
+  sed -i -r "s/^1\t/${i}\t/g" ${prefix}_chrom_${i}.vcf
+  grep ";MT=1\|;MT=2" ${prefix}_chrom_${i}.vcf >> ${prefix}.vcf
+done
+
+# INPUT ARGUMENTS
+sample_size=50
+prefix="../Data/${s_value}/sample_${sample_size}" # Output prefix, and input prefix of given vcf
+
+# Merge all `.vcf` files.
+
+cp ${prefix}_chrom_1.vcf ${prefix}.vcf
+
+for i in {2..400}
+do
+  sed -i -r "s/^1\t/${i}\t/g" ${prefix}_chrom_${i}.vcf
+  grep ";MT=1\|;MT=2" ${prefix}_chrom_${i}.vcf >> ${prefix}.vcf
+done
+
+# INPUT ARGUMENTS
+sample_size=100
+prefix="../Data/${s_value}/sample_${sample_size}" # Output prefix, and input prefix of given vcf
+
+# Merge all `.vcf` files.
+
+cp ${prefix}_chrom_1.vcf ${prefix}.vcf
+
+for i in {2..400}
+do
+  sed -i -r "s/^1\t/${i}\t/g" ${prefix}_chrom_${i}.vcf
+  grep ";MT=1\|;MT=2" ${prefix}_chrom_${i}.vcf >> ${prefix}.vcf
+done
+
+# INPUT ARGUMENTS
+sample_size=500
+prefix="../Data/${s_value}/sample_${sample_size}" # Output prefix, and input prefix of given vcf
+
+# Merge all `.vcf` files.
+
+cp ${prefix}_chrom_1.vcf ${prefix}.vcf
+
+for i in {2..400}
+do
+  sed -i -r "s/^1\t/${i}\t/g" ${prefix}_chrom_${i}.vcf
+  grep ";MT=1\|;MT=2" ${prefix}_chrom_${i}.vcf >> ${prefix}.vcf
+done
+
+# INPUT ARGUMENTS
+sample_size=1000
+prefix="../Data/${s_value}/sample_${sample_size}" # Output prefix, and input prefix of given vcf
+
+# Merge all `.vcf` files.
+
+cp ${prefix}_chrom_1.vcf ${prefix}.vcf
+
+for i in {2..400}
+do
+  sed -i -r "s/^1\t/${i}\t/g" ${prefix}_chrom_${i}.vcf
+  grep ";MT=1\|;MT=2" ${prefix}_chrom_${i}.vcf >> ${prefix}.vcf
+done
+
+# INPUT ARGUMENTS
+s_value=s_0_seed_3
+sample_size=10
+prefix="../Data/${s_value}/sample_${sample_size}" # Output prefix, and input prefix of given vcf
+
+# Merge all `.vcf` files.
+
+cp ${prefix}_chrom_1.vcf ${prefix}.vcf
+
+for i in {2..400}
+do
+  sed -i -r "s/^1\t/${i}\t/g" ${prefix}_chrom_${i}.vcf
+  grep ";MT=1\|;MT=2" ${prefix}_chrom_${i}.vcf >> ${prefix}.vcf
+done
+
+# INPUT ARGUMENTS
+sample_size=50
+prefix="../Data/${s_value}/sample_${sample_size}" # Output prefix, and input prefix of given vcf
+
+# Merge all `.vcf` files.
+
+cp ${prefix}_chrom_1.vcf ${prefix}.vcf
+
+for i in {2..400}
+do
+  sed -i -r "s/^1\t/${i}\t/g" ${prefix}_chrom_${i}.vcf
+  grep ";MT=1\|;MT=2" ${prefix}_chrom_${i}.vcf >> ${prefix}.vcf
+done
+
+# INPUT ARGUMENTS
+sample_size=100
+prefix="../Data/${s_value}/sample_${sample_size}" # Output prefix, and input prefix of given vcf
+
+# Merge all `.vcf` files.
+
+cp ${prefix}_chrom_1.vcf ${prefix}.vcf
+
+for i in {2..400}
+do
+  sed -i -r "s/^1\t/${i}\t/g" ${prefix}_chrom_${i}.vcf
+  grep ";MT=1\|;MT=2" ${prefix}_chrom_${i}.vcf >> ${prefix}.vcf
+done
+
+# INPUT ARGUMENTS
+sample_size=500
+prefix="../Data/${s_value}/sample_${sample_size}" # Output prefix, and input prefix of given vcf
+
+# Merge all `.vcf` files.
+
+cp ${prefix}_chrom_1.vcf ${prefix}.vcf
+
+for i in {2..400}
+do
+  sed -i -r "s/^1\t/${i}\t/g" ${prefix}_chrom_${i}.vcf
+  grep ";MT=1\|;MT=2" ${prefix}_chrom_${i}.vcf >> ${prefix}.vcf
+done
+
+# INPUT ARGUMENTS
+sample_size=1000
+prefix="../Data/${s_value}/sample_${sample_size}" # Output prefix, and input prefix of given vcf
+
+# Merge all `.vcf` files.
+
+cp ${prefix}_chrom_1.vcf ${prefix}.vcf
+
+for i in {2..400}
+do
+  sed -i -r "s/^1\t/${i}\t/g" ${prefix}_chrom_${i}.vcf
+  grep ";MT=1\|;MT=2" ${prefix}_chrom_${i}.vcf >> ${prefix}.vcf
+done
