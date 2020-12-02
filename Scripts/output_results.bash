6896ebc256fabc04df3e0bd07d7514ce302ca9e3
jon-mah <jonmah@uw.edu>
2020-12-02 12:06:42 -0800

    analysis outputs.

--- Scripts/output_results.bash
+++ Scripts/output_results.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
-#$ -t 1-400:1
 #$ -cwd
 #$ -V
+#$ -m bea
 #$ -l h_data=10G
 #$ -l h_rt=1:00:00
 
 # INPUT ARGUMENTS
-s_value=s_0_rec
+s_value=s_0_1_rec
 
 # s_0_rec
 python compute_HW_departure.py ../Data/${s_value}/sample_10.vcf
