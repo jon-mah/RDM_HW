63f114b3e3ef1a530f9c818433ca2135673fb8ce
jon-mah <jonmah@uw.edu>
2020-12-14 10:04:15 -0800

    expected value output.

--- Scripts/output_results.bash
+++ Scripts/output_results.bash
@@ -2,9 +2,8 @@
 #$ -cwd
 #$ -t 2-1000:1
 #$ -V
-#$ -m bea
 #$ -l h_data=10G
-#$ -l h_rt=1:00:00
+#$ -l h_rt=00:05:00
 
 # INPUT ARGUMENTS
 s_value=s_0_rec
@@ -18,11 +17,14 @@ s_value=s_0_rec
 # python compute_HW_departure.py ../Data/${s_value}/sample_5000.vcf
 # python compute_HW_departure.py ../Data/${s_value}/sample_10000.vcf
 
-python compute_HW_expectation.py 10000 $SGE_TASK_ID
+python3 compute_HW_expectation.py 10000 $SGE_TASK_ID
 
 # Hardy Weinberg Expectation
 # for i in {2..200}
 # do
 #   popsize=${i/2+1}
 #   python compute_HW_expectation.py ${popsize} ${i}
-d# one
+# done
+
+rm -rf *.e*
+rm -rf *.o*
